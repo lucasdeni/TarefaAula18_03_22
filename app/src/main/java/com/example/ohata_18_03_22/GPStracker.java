7575590d8cfee1fec1a57499b72cdab563d832de
Aluno <lucasfdenipoti@gmail.com>
2022-03-18 21:33:52 -0300

    Ajustes no código

--- app/src/main/java/com/example/ohata_18_03_22/GPStracker.java
+++ app/src/main/java/com/example/ohata_18_03_22/GPStracker.java
@@ -3,7 +3,7 @@ package com.example.ohata_18_03_22;
 import android.app.Application;
 import android.location.Location;
 
-public class GPStracker {
+public class GPStracker{
     public GPStracker(Application application) {
     }
 
--- app/src/main/java/com/example/ohata_18_03_22/MainActivity.java
+++ app/src/main/java/com/example/ohata_18_03_22/MainActivity.java
@@ -1,26 +1,22 @@
 package com.example.ohata_18_03_22;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import android.app.Application;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.Manifest;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-//Criando classe para rastrear no gps
-class GPStacker {
-    public GPStacker(Application application) {
-    }
-
-    public Location getLocation() {
-        return null;
-    }
-
-    public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
         //Criando as variáveis para imagem e botões
         private ImageView imageViewFoto;
@@ -43,8 +39,46 @@ class GPStacker {
                 public void onClick(View view) {
                     GPStracker g = new GPStracker(getApplication());
                     Location l = g.getLocation();
+
+                    //Com l vazia, ele pegará os dados de lat e lon e exibirá para o usuário
+                    if(l != null) {
+                        double lat = l.getLatitude();
+                        double lon = l.getLongitude();
+                        Toast.makeText(getApplicationContext(), "LATITUDE: " + lat +"\n LONGITUDE: " + lon, Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+            //Superclasse checa a permissão para a utilização da câmera
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 0);
+            }
+
+            //Guarda a foto ao tirar ela
+            imageViewFoto = (ImageView) findViewById(R.id.Foto);
+            findViewById(R.id.btn_ft).setOnClickListener(new View.OnClickListener() {
+                //Novo método sendo utilizado
+                @Override
+                public void onClick(View view) {
+                    tirarFoto();
                 }
             });
         }
+
+        //Permissão para armazenar a foto e exibir ela pro usuário
+    private void tirarFoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    //Criando método de SuperClasse
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //Ao tirar uma foto com sucesso, mostre ela
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imagem = (Bitmap) extras.get("data");
+            imageViewFoto.setImageBitmap(imagem);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
